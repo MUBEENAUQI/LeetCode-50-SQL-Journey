3cbb72d044cd09a0cc17b91ba5ce1031f103f6fb
Mubeena Kabeer <49668351+MUBEENAUQI@users.noreply.github.com>
2025-11-30 22:27:01 +0530

    Time: 382 ms (80.09%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,6 +1,7 @@
 # Write your MySQL query statement below
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2
-  ON w1.recordDate = w2.recordDate + INTERVAL 1 DAY
-WHERE w1.temperature > w2.temperature
+select W1.id 
+from Weather W1
+left join Weather W2
+on W1.recordDate = Date_ADD(W2.recordDate, INTERVAL 1 DAY)
+where W1.temperature > W2.temperature
+
